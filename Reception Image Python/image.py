42e2a51d05fa8a272c6742c4492acd5e5409f4bd
Olivier <olivierbetil@gmail.com>
2022-12-21 19:24:50 +0100

    Ajout du script python
    permet de récupérer l'info et de la transformer en une image

---
+++ Reception Image Python/image.py
@@ -0,0 +1,76 @@
+#**********************************************************************************************************
+#   @file image.py
+#   @author Olivier BETIL
+#   @brief enregistre une image arrivant dans un port serial dans un fichier .txt
+#   @date 21/12/2022
+#**********************************************************************************************************
+
+#Utilisation des librairies:
+#   PySerial: Lecture des ports serial de l'ordinateur
+#   Numpy: gestion de matrices
+import serial
+import numpy as np
+
+#Initialisation et ouverture du port COM
+ser = serial.Serial()
+ser.baudrate = 115200
+ser.port = 'COM14'
+ser.open()
+
+
+#boucle pour trouver le début d'une image (la stm envoie "helloworldhelloworldhelloworldxx" entre chaque image)
+test=1
+while(test==1):
+    temp = str(ser.read(20))
+    if "helloworld" in temp:
+        test=0
+
+#on s'assure de terminer le message inter-image avant de stocker l'image
+test=1
+while(test==1):
+    temp=ser.read(1)
+    if(str(temp)=="b'd'"):
+        if(str(ser.read(1))=="b'x'"):
+            if(str(ser.read(1))=="b'x'"):
+                test=0
+
+#création de la matrice qui représente les deux octets pour un pixel
+imagearray = np.empty((120, 160, 2), dtype=bytes)
+
+#init
+image=""
+compteur = 0
+
+
+#On préviens l'utilisateur qu' l'on commence à enregistrer une image
+print("Enregistrement d'une image")
+
+#on lit chaque pixel reçu dans le port serial
+for y in range(120):
+    for x in range(160):
+        imagearray[y][x][0]=ser.read(1)
+        imagearray[y][x][1]=ser.read(1)
+
+#fermeture du port COM
+ser.close()
+
+#transformation de la matrice en une chaine pour enregistrer dans un fichier .txt
+for y in range(120):
+    for x in range(160):
+        if(imagearray[y][x][0]=="b''"):
+            imx=0
+        else:
+            imx=imagearray[y][x][0]
+        if(imagearray[y][x][1]=="b''"):
+            imy=0
+        else:
+            imy=imagearray[y][x][1]
+        image+=str(imx)+"  "+str(imy)+"\n"
+
+
+
+
+#enregistre dans un fichier .txt les valeurs reçues
+doc = open("test.txt", "w", encoding='utf-8')
+doc.write(image)
+doc.close()
---
+++ Reception Image Python/pil.py
@@ -0,0 +1,58 @@
+#**********************************************************************************************************
+#   @file pil.py
+#   @author Betil Olivier
+#   @brief script qui récupère les données d'une image stockées dans un fichier .txt et reconstruit l'image
+#   @date 21/12/2022
+#**********************************************************************************************************
+
+#utilisation des librairies:
+#   Numpy: gestion de matrices
+#   PIL: gestion d'images
+#   Bitstring: gestion de trames de bits
+import numpy as np
+from PIL import Image
+from bitstring import BitArray
+
+#Fichier où se trouve les données pour l'image
+doc = open("test.txt", "r", encoding='utf-8')
+
+#création de la matrice qui représente l'image (np.uint8 car c'est le format à utiliser pour l'image)
+imagearray = np.empty((120, 160, 3), dtype=np.uint8)
+
+#Init
+x=0
+y=0
+
+#Boucle qui va faire tout le fichier
+for line in doc:
+
+    #récupère en trame de bits les données de l'image
+    pixel=line.split("  ")
+    try:
+        b=BitArray(hex="0"+pixel[0][3:6])
+    except:
+        b=BitArray(hex=hex(ord(pixel[0][2])))
+    try:
+        b+=BitArray(hex="0"+pixel[1][3:6])
+    except:
+        b+=BitArray(hex=hex(ord(pixel[1][2])))
+    
+    #récupère chaque couleur dans la trame, et passe en int
+    rouge = b[:5].uint
+    vert = b[5:11].uint
+    bleu=b[11:].uint
+
+    #Stocke les valeurs dans la matrice
+    imagearray[y][x][0]=rouge
+    imagearray[y][x][1]=vert/2
+    imagearray[y][x][2]=bleu
+
+    #Incrémentation (ligne par ligne, comme envoyé par stm)
+    x+=1
+    if(x==160):
+        x=0
+        y+=1
+
+#enregistre l'image en format jpg depuis la matrice
+pil_image = Image.fromarray(imagearray, mode="RGB")
+pil_image.save("image.jpg")
